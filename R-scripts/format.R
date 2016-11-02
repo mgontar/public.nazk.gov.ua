1e2d5bf4e70ce0bc97c2bab25a476bb6af2a527f
Maksym Gontar <max.gontar@gmail.com>
2016-11-02 10:48:25 +0200

    Add json support

--- R-scripts/format.R
+++ R-scripts/format.R
@@ -3,7 +3,7 @@ allpersons <- read.csv("allpersons.csv")
 
 allpersons$urlhtmlp <- paste("https://public.nazk.gov.ua/declaration/", allpersons$id, sep="")
 allpersons$urlpdfp <- paste("https://public.nazk.gov.ua/storage/documents/pdf/", allpersons$pdf, sep="")
-
+allpersons$urljsonp <- paste("https://public-api.nazk.gov.ua/v1/declaration/", allpersons$id, sep="")
 trformat <- "<tr><td class='tdname'>%s</td><td class='tdpos'>%s</td><td class='tdurlhtml'><a href='%s'>html</a></td><td class='tdurlpdf'><a href='%s'>pdf</a></td></tr>"
 allpersons$html <- sprintf(trformat, allpersons$name, allpersons$pos, allpersons$urlhtmlp, allpersons$urlpdfp)
 
@@ -14,6 +14,7 @@ write.table(urlhtmlall, file = "urls.txt", col.names = F, row.names = F, quote =
 
 write.table(allpersons$urlhtmlp, file = "html_url_list.txt", col.names = F, row.names = F, quote = F)
 write.table(allpersons$urlpdfp, file = "pdf_url_list.txt", col.names = F, row.names = F, quote = F)
+write.table(allpersons$urljsonp, file = "json_url_list.txt", col.names = F, row.names = F, quote = F)
 
 htmltable <- allpersons$html
 
